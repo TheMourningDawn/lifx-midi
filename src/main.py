088b25d2f7f2c5f5e1ec96c5deea0920a595cd56
Charlie Hendricks <charlie.hendricks@equipmentshare.com>
2021-03-08 20:24:05 -0600

    Got lifx and a midi input working generally

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import sys
 
-from lifxlan import LifxLAN
+from lifxlan import LifxLAN, Light
+
+import rtmidi
 
 
 def main():
-    num_lights = None
+    num_lights = 12
     if len(sys.argv) != 2:
         print("\nDiscovery will go much faster if you provide the number of lights on your LAN:")
         print("  python {} <number of lights on LAN>\n".format(sys.argv[0]))
@@ -26,6 +28,35 @@ def main():
             print(d)
         except Exception:
             pass
+
+    right: Light = Light("d0:73:d5:20:e4:c6", "10.0.0.27")
+
+    right.set_power(True)
+
+    midiin = rtmidi.RtMidiIn()
+
+    def print_message(midi):
+        if midi.isNoteOn():
+            print('ON: ', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
+        elif midi.isNoteOff():
+            print('OFF:', midi.getMidiNoteName(midi.getNoteNumber()))
+        elif midi.isController():
+            print('CONTROLLER', midi.getControllerNumber(), midi.getControllerValue())
+
+    ports = range(midiin.getPortCount())
+    if ports:
+        for i in ports:
+            print(midiin.getPortName(i))
+        print("Opening port 0!")
+        midiin.openPort(0)
+        while True:
+            m = midiin.getMessage(250)  # some timeout in ms
+            if m and m.isNoteOn():
+                right.set_brightness(m.getVelocity() * 512)
+                print(f'{m.getVelocity()} {m.getMidiNoteName(m.getNoteNumber())}')
+                # print_message(m)
+    else:
+        print('NO MIDI INPUT PORTS!')
 
 
 if __name__ == "__main__":
